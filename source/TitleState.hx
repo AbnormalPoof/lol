e04c7d252afaf58c245c55cd547dbcbdda745dc0
AbnormalPoof <userabnormal89@gmail.com>
2022-05-19 23:06:04 -0500

    auto commit idk

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -162,7 +162,7 @@ class TitleState extends MusicBeatState
 		gfDance.shader = swagShader.shader;
 		add(logo);
 		
-		mikuDance = new FlxSprite(FlxG.width * 0.5, FlxG.height * 0.08);
+		mikuDance = new FlxSprite(FlxG.width * 0.48, FlxG.height * 0.1);
 		mikuDance.frames = Paths.getSparrowAtlas('mikuDance');
 		mikuDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		mikuDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
